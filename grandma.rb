1abd5cf2add26bb067f3131bd0b672d35da82576
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-09 21:32:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,13 +2,13 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-    elsif phrase != phrase.capitalize
+    elsif phrase != phrase.upcase
      return "HUH?! SPEAK UP, SONNY!"
      
-    elsif phrase != phrase.capitalize
+    elsif phrase != phrase.upcase
      return "HUH?! SPEAK UP, SONNY!"
      
-    elsif phrase == phrase.capitalize
+    elsif phrase == phrase.upcase
      return "NO, NOT SINCE 1938!"
   end
 end
